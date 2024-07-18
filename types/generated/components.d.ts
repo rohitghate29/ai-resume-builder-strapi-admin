a02141b68a744d28597815e946a1d9ed1e7cf8df
Rohit <rohitghate78692@gmail.com>
2024-07-18 22:01:03 +0530

    resolved bugs

--- types/generated/components.d.ts
+++ types/generated/components.d.ts
@@ -1,22 +1,11 @@
 import type { Struct, Schema } from '@strapi/strapi';
 
-export interface SkillsSkills extends Struct.ComponentSchema {
-  collectionName: 'components_skills_skills';
-  info: {
-    displayName: 'Skills';
-    icon: 'cup';
-  };
-  attributes: {
-    name: Schema.Attribute.String;
-    rating: Schema.Attribute.Integer;
-  };
-}
-
 export interface ExperienceExperience extends Struct.ComponentSchema {
   collectionName: 'components_experience_experiences';
   info: {
     displayName: 'Experience';
     icon: 'clock';
+    description: '';
   };
   attributes: {
     title: Schema.Attribute.String;
@@ -25,7 +14,7 @@ export interface ExperienceExperience extends Struct.ComponentSchema {
     state: Schema.Attribute.String;
     startDate: Schema.Attribute.String;
     endDate: Schema.Attribute.String;
-    workSummary: Schema.Attribute.String;
+    workSummary: Schema.Attribute.Text;
   };
 }
 
@@ -45,12 +34,24 @@ export interface EducationEducation extends Struct.ComponentSchema {
   };
 }
 
+export interface SkillsSkills extends Struct.ComponentSchema {
+  collectionName: 'components_skills_skills';
+  info: {
+    displayName: 'Skills';
+    icon: 'cup';
+  };
+  attributes: {
+    name: Schema.Attribute.String;
+    rating: Schema.Attribute.Integer;
+  };
+}
+
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
-      'skills.skills': SkillsSkills;
       'experience.experience': ExperienceExperience;
       'education.education': EducationEducation;
+      'skills.skills': SkillsSkills;
     }
   }
 }
